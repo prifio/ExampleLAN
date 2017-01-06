0e2d38e28431ce8664715294a7a82b062b2af321
prifio <tolya_ash@mail.ru>
2017-01-06 20:02:22 +0000

    Some changes

--- Server/Pr2/Program.cs
+++ Server/Pr2/Program.cs
@@ -12,38 +12,38 @@ namespace ConsoleApplication1
 {
     class Server
     {
-        //public Queue<int> q { get; private set; }
         public Socket SocketList, SocketSend;
-        private Mutex mutOut;
-        private int Send = 0, Receive = 0;
+        
         public Task<int> TryReceiveAsync(byte[] buffer, int offset, int size, SocketFlags flags)
         {
-            return Task<int>.Factory.FromAsync(
-                SocketSend.BeginReceive(buffer, offset, size, flags, null, SocketSend), SocketSend.EndReceive);
+            TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
+            SocketSend.BeginReceive(buffer, offset, size, flags, iar =>
+            {
+                tcs.SetResult(SocketSend.EndReceive(iar));
+            }, SocketSend);
+            return tcs.Task;
         }
         public async Task<string> ReceiveAsync()
         {
             int size = 0;
-            //int remaining = buffer.Length;
             byte[] data = new byte[1024];
             while (true)
             {
                 var received = await TryReceiveAsync(data, size, 1024 - size, SocketFlags.None);
                 size += received;
-                //Console.WriteLine(Encoding.ASCII.GetString(data, 0, size));
-                string result = Encoding.ASCII.GetString(data, 0, size);
-                int IndOf = result.IndexOf('\n');
-                if (IndOf != -1)
-                    return result.Substring(0, IndOf);
+                if (data[size - 1] == '\n')
+                    return Encoding.ASCII.GetString(data, 0, size - 1);
                 Console.WriteLine(received);
-                //return result.Substring(0, result.Length - 1);
             }
         }
-        
         public Task<int> TrySendAsync(byte[] buffer, int offset, int size, SocketFlags flags)
         {
-            return Task<int>.Factory.FromAsync(
-                SocketSend.BeginSend(buffer, offset, size, flags, null, SocketList), SocketSend.EndReceive);
+            TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
+            SocketSend.BeginSend(buffer, offset, size, flags, iar =>
+            {
+                tcs.SetResult(SocketSend.EndSend(iar));
+            }, SocketSend);
+            return tcs.Task;
         }
         public async Task SendAsync(string s)
         {
@@ -57,35 +57,29 @@ namespace ConsoleApplication1
                 left -= sended;
             }
         }
-        public void Create(string IP)
-        {
-            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(IP), 11000);
-            SocketList.Bind(ip);
-            SocketList.Listen(20);
-            Console.WriteLine("endCreate");
-        }
         public async Task InfReceive()
         {
             while (true)
             {
                 string s = await ReceiveAsync();
-                mutOut.WaitOne();
-                ++Receive;
-                Console.WriteLine("\t\t\t\tYour frined: " + s + " " + Receive + " " + Send);
-                mutOut.ReleaseMutex();
+                Console.WriteLine("\t\t\t\tYour frined: " + s);
             }
         }
         public async Task InfSend()
         {
             while (true)
             {
-                string s = "123";// Console.ReadLine();
+                string s = Console.ReadLine();
                 await SendAsync(s);
-                mutOut.WaitOne();
-                ++Send;
-                Console.WriteLine("\t\t\t\tYou: " + s + " " + " " + Receive + " " + Send);
-                mutOut.ReleaseMutex();
+                Console.WriteLine("\t\t\t\tYou: " + s);
             }
+        }
+        public void Create(string IP)
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(IP), 11000);
+            SocketList.Bind(ip);
+            SocketList.Listen(20);
+            Console.WriteLine("endCreate");
         }
         public Task<Socket> Accept()
         {
@@ -95,13 +89,12 @@ namespace ConsoleApplication1
         public Server()
         {
             SocketList = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            mutOut = new Mutex();
         }
     }
 
     class A
     {
-        const string IP = "192.168.1.48";
+        const string IP = "192.168.2.220";
         public async Task run()
         {
             Server server = new Server();
